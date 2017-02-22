60614c0401272ccabf3baeee778614c72407bd4a
Alvaco <vas09284@uvg.edu.gt>
2017-02-22 11:14:18 -0600

    a

---
+++ src/iStack.java
@@ -0,0 +1,20 @@
+
+public interface iStack<E>{
+	
+	/**
+	 * @param algo
+	 * Ingresa un elemento a un objeto
+	 */
+	public void push(E algo);
+	
+	/**
+	 * @return un elemento del final de una lsita
+	 */
+	public E pop();
+	
+	/**
+	 * @return true si el objeto está vacío
+	 */
+	public boolean isEmpty();
+	
+}

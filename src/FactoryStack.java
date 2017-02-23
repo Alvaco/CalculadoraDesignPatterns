1d67936b5bd73e2bcfc4d375a5d32f589ddf69d9
LEPPEDIAZ <LEPPEDIAZ@users.noreply.github.com>
2017-02-23 14:25:11 -0300

    Update FactoryStack.java

--- src/FactoryStack.java
+++ src/FactoryStack.java
@@ -1,12 +1,50 @@
+import java.util.Vector;
 
-public class FactoryStack <E>{
-	//selecciona la implementacion a utilizar para un stack
-	//se utiliza el patron Factory
-	   public iStack<E> getStack(String entry) {
-	    // seleccion de la implementacion a utilizar:
-		if (entry.equals("AL"))
-	      return new ArrayListStack<E>(); //regresa ArrayList
-		else
-	      return new VectorStack<E>(); //regresa Vector
-	   }
+public class FactoryStack <E> 
+implements Stack<E>{
+    
+    protected Vector<E> data;
+
+        public StackVector (){
+            data = new Vector<E>();
+        }
+    
+        @Override
+	public void push(E item)
+	// post: the value is added to the stack
+	// will be popped next if no intervening push
+	{
+		data.addElement(item);
 	}
+
+        @Override
+	public E pop()
+	// pre: stack is not empty
+	// post: most recently pushed item is removed and returned
+	{
+		return data.remove(size()-1);
+	}
+
+        @Override
+	public E peek()
+	// pre: stack is not empty
+	// post: top value (next to be popped) is returned
+	{
+		return data.get(size() - 1);
+	}
+	
+        @Override
+	public int size()
+	// post: returns the number of elements in the stack
+	{
+		return data.size();
+	}
+  
+        @Override
+	public boolean empty()
+	// post: returns true if and only if the stack is empty
+	{
+		return size() == 0;
+	}
+    
+}
